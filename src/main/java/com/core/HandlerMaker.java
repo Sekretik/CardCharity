aa14c65ba744b4071eeeea5c1873767e9c83f2f3
SimpleLavr <SimpleLavr@github.com>
2021-10-04 12:45:50 +0500

    Fixed lowercase

--- src/main/java/com/core/HandlerMaker.java
+++ src/main/java/com/core/HandlerMaker.java
@@ -196,7 +196,7 @@ class CardHandler extends AbstractHandler {
     public void handle(String uri, Request request, HttpServletRequest httpServletRequest, HttpServletResponse response) throws IOException, ServletException {
         Logger logger = LoggerFactory.getLogger("CardHandlerLogger");
         request.setHandled(true);
-        response.addHeader("Access-Control-Allow-Origin", "https://github.com");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         response.setContentType("application/json; charset=UTF-8");
         String originalURI = request.getOriginalURI();
 
@@ -387,7 +387,7 @@ class ShopHandler extends AbstractHandler {
         Logger logger = LoggerFactory.getLogger("ShopLogger");
         request.setHandled(true);
         response.setContentType("application/json; charset=UTF-8");
-        response.addHeader("Access-Control-Allow-Origin", "https://github.com");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         String originalURI = request.getOriginalURI();
 
         switch (request.getMethod()) {
@@ -461,7 +461,7 @@ class CodeHandler extends AbstractHandler {
         request.setHandled(true);
         response.setStatus(200);
         response.setContentType("image/png");
-        response.addHeader("Access-Control-Allow-Origin", "https://github.com");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         String originalURI = request.getOriginalURI();
         codeLogger.debug("Getting code image, URI: {}", originalURI);
         try {
