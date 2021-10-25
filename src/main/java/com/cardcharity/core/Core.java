a594666502c7e35c7e1070ec2a620b9c16527021
Sekretik <Sekretik@github.com>
2021-10-25 09:49:19 +0500

    123

--- src/main/java/com/cardcharity/core/Core.java
+++ src/main/java/com/cardcharity/core/Core.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("com.cardcharity")
+@PropertySource("configs.properties")
 public class Core {
     public static ApplicationContext context;
 
     public static void main(String[] args) {
-        context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+        context = new AnnotationConfigApplicationContext(Core.class);
         SpringApplication.run(Core.class, args);
     }
 }
--- src/main/java/com/cardcharity/web/exception/Response.java
+++ src/main/java/com/cardcharity/web/exception/Response.java
@@ -1,20 +1,20 @@
 package com.cardcharity.web.exception;
 
 public class Response {
-    private String message;
+    private String error;
 
     public Response() {
     }
 
-    public Response(String message) {
-        this.message = message;
+    public Response(String error) {
+        this.error = error;
     }
 
-    public String getMessage() {
-        return message;
+    public String getError() {
+        return error;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setError(String error) {
+        this.error = error;
     }
 }
